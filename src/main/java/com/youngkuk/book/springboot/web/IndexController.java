f61e0c47b1d32152740d3512ef3525ad29e4ee7d
0guk <yogusi91@gmailc.om>
2022-06-26 19:39:48 +0900

    cache clear

--- src/main/java/com/youngkuk/book/springboot/web/IndexController.java
+++ src/main/java/com/youngkuk/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.youngkuk.book.springboot.web;
 
+import com.youngkuk.book.springboot.web.config.auth.LoginUser;
+import com.youngkuk.book.springboot.web.config.auth.dto.SessionUser;
 import com.youngkuk.book.springboot.web.dto.PostsResponseDto;
 import com.youngkuk.book.springboot.web.service.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -15,10 +19,13 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
 
         model.addAttribute("posts", postsService.findAllDesc());
 
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -35,5 +42,4 @@ public class IndexController {
 
         return "posts-update";
     }
-
 }
---
+++ src/main/java/com/youngkuk/book/springboot/web/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.youngkuk.book.springboot.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
