78e0369a3ded7e6a0186f1f6849e5d29bcfb4332
0guk <yogusi91@gmailc.om>
2022-06-28 22:51:36 +0900

    Travis Test

--- src/main/java/com/youngkuk/book/springboot/web/HelloController.java
+++ src/main/java/com/youngkuk/book/springboot/web/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class HelloController {
 
@@ -16,5 +18,10 @@ public class HelloController {
     @GetMapping("/hello/dto")
     public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
         return new HelloResponseDto(name, amount);
+    }
+
+    @GetMapping("/server-time")
+    public Date helloDto() {
+        return new Date();
     }
 }
